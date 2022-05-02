7c0e43402d7d17902b1cb875736b91317719929a
vmachord <vane_divis@hotmail.es>
2022-05-02 23:03:32 +0200

    Subir Ejercicios Python

---
+++ Ejercicios/EjerciciosPropuestos.py
@@ -0,0 +1,168 @@
+from cmath import pi
+
+
+try: 
+    salir = False
+    opcion = 0
+    while (not salir):
+        print("\n")
+        print ("1. Calcular y mostrar el cuadrado de los números del 1 a 30")
+        print ("2. Números primos")
+        print ("3. Construir un avión de papel")
+        print ("4. Realizar las cuatro operaciones básicas")
+        print ("5. Volumen y Area de un Cilindro")
+        print ("6. Pedir un libro en una biblioteca")
+        print ("7. Encontrar el mayor número de tres números")
+        print ("8. Factorial de cualquier número")
+        print ("9. Encontrar si un numero en mayor o menor a un número dado")
+        print ("10. Adivinar una palabra")
+
+        print ("11. Salir")
+
+        print ("Elige una opcion")
+        print("\n")
+
+        opcion = int(input())
+    
+        if opcion == 1:
+            #1. Calcular y mostrar el cuadrado de los números del 1 a 30
+            for numero in range(1,31):
+                print(f"El cuadrado de {numero} es: {numero*numero}")
+
+        elif opcion == 2:
+            #2. Números primos
+            num = input("Ingrese un numero: ")
+            if(num.isdigit()):
+                div=2
+                band=True
+                num = int(num)
+                if(num==1):
+                    print("Es primo \n")
+                else:
+                    #& band==True 
+                    while(band==True and num>div ): 
+                        if(num % div ==0):
+                            band=False
+                        div+=1
+                    if(band):
+                        print(f"{num} es primo \n")
+                    else:
+                        print(f"{num} no es primo \n")
+            else:   
+                print("Por Favor, ingresar un valor valido")  
+
+        elif opcion == 3:
+            #3. Construir un avión de papel
+            print("Paso 1: Conseguir Hoja de Papel") 
+            print("Paso 2: Doblar el papel a la mitad") 
+            print("Paso 3: Doblar la esquina al centro izquierdo") 
+            print("Paso 4: Volver a doblar el papel a la mitad") 
+            print("Paso 5: Con el papel doblado volver a doblar la esquina al centro izquierdo") 
+            print("Paso 6: Doblar hacia abajo para formar alas en ambos lados") 
+            print("Paso 7: Comprobar los pasos anteriores") 
+            print("Paso 8: (Opcional) Probar que vuela")
+
+        elif opcion == 4:
+            #4. Realizar las cuatro operaciones básicas
+            num1 = input("Ingrese un numero: ")
+            num2 = input("Ingrese otro numero: ")
+            try:
+                num1=float(num1)
+                num2=float(num2)
+                print(f"La suma es: {num1+num2}")
+                print(f"La resta es: {num1-num2}")
+                print(f"La multiplicacion es: {num1*num2}")
+                while(num2==0):
+                    num2 = float(input("No se puede realizar una division por cero, por favor ingrese otro numero:"))
+                print(f"La division es: {num1/num2}")
+            except:   
+                print("Por Favor, ingresar valores numericos validos") 
+
+        elif opcion == 5:
+            #5. Volumen y Area de un Cilindro
+            radio = input("Ingrese el radio del cilindro: ")
+            altura = input("Ingrese la altura del cilindro: ")
+            if(radio.isdigit() and altura.isdigit()):
+                radio=float(radio)
+                altura=float(altura)
+                area=2*(pi*pow(radio, 2) ) + 2*pi*radio*altura
+                volumen=pi*pow(radio, 2)*altura 
+                print(f"El area del cilindro es: {area} y el volumen: {volumen}")
+            else:   
+                print("Por Favor, ingresar valores validos")   
+
+        elif opcion == 6:
+            #6. Pedir un libro en una biblioteca
+            print("Paso 1: Ir hasta la Biblioteca") 
+            print("Paso 2: Hablar con la Bibliotecaria") 
+            print("Paso 3: Informar cual libro quiere llevar") 
+            print("Paso 4: Esperar que la bibliotecaria encuentre el libro") 
+            print("Paso 5: Mostrar la tarjeta de la Bibliotca a la Bibliotecaria") 
+            print("Paso 6: Recoger el libro") 
+            print("Paso 7: Volver a casa con el libro") 
+
+        elif opcion == 7:
+            #7. Encontrar el mayor número de tres números
+            num1 = input("Ingrese el primer numero: ")
+            num2 = input("Ingrese el segundo numero: ") 
+            num3 = input("Ingrese el tercer numero: ") 
+            if(num1.isdigit() and num2.isdigit() and num3.isdigit()):
+                if (num1 >= num2 and num1>= num3):
+                    print(f"El mayor numero es: {num1}")
+                elif (num2>=num1 and num2>=num3):
+                    print(f"El mayor numero es: {num2}")
+                else:
+                    print(f"El mayor numero es: {num3}")
+            else:   
+                print("Por Favor, ingresar valores validos") 
+
+        elif opcion == 8:
+            #8. Factorial de cualquier número
+            num = input("Ingrese un numero: ")
+            if(num.isdigit()):
+                num=int(num)
+                fact=1
+                for numero in range(1,num+1):
+                    fact=fact*numero
+                print(f"El factorial de {num} es: {fact}")
+            else:   
+                print("Por Favor, ingresar un numero valido")  
+
+        elif opcion == 9:
+            #9. Encontrar si un numero en mayor o menor a un número dado
+            NumeroDado=30
+            num = input("Ingrese un numero: ")
+            if(num.isdigit()):
+                num=int(num)
+                if(num>NumeroDado):
+                    print(f"Su numero es mayor a {NumeroDado}")
+                elif(num<NumeroDado):
+                    print(f"Su numero es menor a {NumeroDado}")
+                else:
+                    print(f"Su numero es igual a {NumeroDado}")
+            else:   
+                print("Por Favor, ingresar un numero valido") 
+
+        elif opcion == 10:
+            #10. Adivinar una palabra
+            palabra = "fiesta"
+            r = "si"
+            while(r == "si"):
+                palabraEntrada = input("Entre la palabra a adivinar: ")
+                if (palabra == palabraEntrada):
+                    print("Has adivinado!!!")
+                    break
+                else:
+                    r = input("No has adivinado. Otra oportunidad (si/no)?")   
+        elif opcion == 11:
+            #11. Salir
+            salir = True        
+        else:
+            print ("Introduce un numero entre 1 y 11 \n")
+except:
+    print ('Ocurrio un Error')
+else:
+    print("Las instrucciones se completaron en su totalidad")
+finally:
+    print('Fin del programa')    
+
--- Ejercicios/EjerciciosPython.py
+++ Ejercicios/EjerciciosPython.py
@@ -29,27 +29,38 @@ try:
 
         elif opcion == 2:
             #2. A partir de un numero ingresado diga si es mayor o menor o igual a 9
-            num = int(input("Ingrese un numero "))
-            if(num > 9):
-                print(f"{num} es mayor a 9 \n")
-            elif(num < 9):
-                print(f"{num} es menor a 9 \n")
+            num = input("Ingrese un numero ")
+            if(num.isdigit()):
+                num=int(num)
+                if(num > 9):
+                    print(f"{num} es mayor a 9 \n")
+                elif(num < 9):
+                    print(f"{num} es menor a 9 \n")
+                else:
+                    print(f"{num} es igual a 9 \n")
             else:
-                print(f"{num} es igual a 9 \n")
+                print("Por favor entre un valor numerico")
 
         elif opcion == 3:
             #3. A partir de un numero ingresado diga si el mismo es par o impar
-            num = int(input("Ingrese un numero "))
-            if(num % 2 ==0):
-                print(f"{num} es par \n")
-            else: 
-                print(f"{num} es impar \n")
+            num = input("Ingrese un numero ")
+            if(num.isdigit()):
+                num=int(num)
+                if(num % 2 ==0):
+                    print(f"{num} es par \n")
+                else: 
+                    print(f"{num} es impar \n")
+            else:
+                print("Por favor entre un valor numerico")
 
         elif opcion == 4:
             #4. Ingresar dos numeros y devuelva el resultado de la suma entre ambos
-            num1 = int(input("Ingrese un numero "))
-            num2 = int(input("Ingrese otro numero "))
-            print(f"La suma de {num1} con {num2} es {num1+num2} \n")
+            num1 = input("Ingrese un numero ")
+            num2 = input("Ingrese otro numero ")
+            if(num1.isdigit() and num2.isdigit()):
+                print(f"La suma de {num1} con {num2} es {int(num1)+int(num2)} \n")
+            else:
+                print("Los valores entrados no son numeros validos")
 
         elif opcion == 5:
             #5. Sumar todo los numeros pares entre 2 y 100
@@ -61,35 +72,59 @@ try:
 
         elif opcion == 6:
             #6. Ingresar un numero y muestre todos los divisores del mismo
-            num = int(input("Ingrese un numero "))
-            for numero in range(1,num+1):
-                if(num %numero==0):
-                    print(numero)
+            num = input("Ingrese un numero ")           
+            if(num.isdigit()):
+                num=int(num)
+                for numero in range(1,num+1):
+                    if(num %numero==0):
+                        print(numero)
+            else:
+                print("Por favor entre un numero valido")
+
             print("\n")
 
         elif opcion == 7:
             #7. Determinar si un alumno aprueba o suspende un curso, sabiendo que aprobara si su
             #   promedio de tres calificaicones es mayor o igual a 4.0; suspende en caso contrario. Debera
             #   permitir ingresar las tres calificaciones y luego calcular su promedio
-            calificacion1 = float(input("Ingrese primer calificacion "))
-            calificacion2 = float(input("Ingrese segunda calificacion "))
-            calificacion3 = float(input("Ingrese tercera calificacion "))
-
-            promedio = (calificacion1 +calificacion2+ calificacion3)/3
-            if(promedio>=4.0):
-                print (f"Promedio:{promedio},Felicidades! Aprobas el curso :D \n")
-            else: 
-                print (f"Promedio:{promedio},Lamentablemente perdes el curso T.T \n")
+            calificacion1 = input("Ingrese primer calificacion: ")
+            calificacion2 = input("Ingrese segunda calificacion: ")
+            calificacion3 = input("Ingrese tercera calificacion: ")
+            try:
+            #if(calificacion1.isdigit() and calificacion2.isdigit() and calificacion3.isdigit()):                        
+                calificacion1=float(calificacion1)
+                calificacion2=float(calificacion2)
+                calificacion3=float(calificacion3)
+                if((calificacion1>=0 and calificacion1<=10)and (calificacion2>=0 and calificacion2<=10) and (calificacion3>=0 and calificacion3<=10)):
+                #if((calificacion1 in range(0,11))and (calificacion2 in range(0,11)) and (calificacion3 in range(0,11))):
+                    promedio = (calificacion1 +calificacion2+calificacion3)/3
+                    if(promedio>=4.0):
+                        print (f"Promedio:{promedio},Felicidades! Aprobas el curso :D \n")
+                    else: 
+                        print (f"Promedio:{promedio},Lamentablemente perdes el curso T.T \n")
+                else:
+                    print("El rango de las calificaciones no esta entre 0 y 10")
+            #else:
+            except:
+                print("Por favor entre calificaciones con numeros enteros")
+            print("\n")
 
         elif opcion == 8:
             #8. Crear un algoritmo que permita ingresar un nombre y una cantidad numerica para escribir
             #    este nombre tantas veces como su cantidad ingresada
-            nombre = input("Ingrese tu nombre ")
-            cantidad = int(input("Ingrese una cantidad "))
-            cont=0
-            while(cont<cantidad): 
-                print(nombre)
-                cont+=1
+            nombre = input("Ingrese tu nombre: ")
+            cantidad = input("Ingrese una cantidad: ")
+            if(not nombre.isdigit()):
+                if(cantidad.isdigit()):
+                    cont=0
+                    cantidad=int(cantidad)
+                    while(cont<cantidad): 
+                        print(nombre)
+                        cont+=1
+                else:
+                    print("Por favor entre una cantidad numerica")
+            else:
+                print("Por favor entre el nombre en texto")
             print("\n")
 
         elif opcion == 9:
@@ -98,44 +133,62 @@ try:
             for numero in range(1,51):
                 suma+=numero
             print(f"La suma de los numeros del 1 a 50 es: {suma} \n")
+            print("\n")
+
         elif opcion == 10:
             #10. Leer tres numeros; si el primero es negativo, debe imprimir la multiplicacion
             #   de los tres y si no lo es, imprimira la suma
-            num1 = int(input("Ingrese primer numero "))
-            num2 = int(input("Ingrese segundo numero "))
-            num3 = int(input("Ingrese tercer numero "))
-            if(num1<0):
-                print(f"La multiplicacion de los numeros es:{num1*num2*num3} \n")
-            else:
-                print(f"La suma de los numeros es:{num1+num2+num3} \n")
+            num1 = input("Ingrese el primer numero: ")
+            num2 = input("Ingrese el segundo numero: ")
+            num3 = input("Ingrese el tercer numero: ")
+            try:
+                num1=int(num1)
+                num2=int(num2)
+                num3=int(num3)
+                if(num1<0):
+                    print(f"La multiplicacion de los numeros es:{num1*num2*num3} \n")
+                else:
+                    print(f"La suma de los numeros es:{num1+num2+num3} \n")
+            except ValueError:
+                print("Por favor, ingrese un valor numerico")
+            except:
+                print("Error")
+            print("\n")
 
         elif opcion == 11:   
             #11. Si un numero ingresado es primo o no. (Un numero es primo si es divisible unicamente por 1
             #   y por si mismo)
-            num = int(input("Ingrese un numero "))
-            div=2
-            band=True
-            if(num==1):
-                print("Es primo \n")
-            else:
-                #& band==True 
-                while(band==True and num>div ): 
-                    if(num % div ==0):
-                        band=False
-                    div+=1
-                if(band):
+            num = input("Ingrese un numero: ")
+            if(num.isdigit()):
+                div=2
+                band=True
+                num = int(num)
+                if(num==1):
                     print("Es primo \n")
                 else:
-                    print("No es primo \n")
-
+                    #& band==True 
+                    while(band==True and num>div ): 
+                        if(num % div ==0):
+                            band=False
+                        div+=1
+                    if(band):
+                        print(f"{num} es primo \n")
+                    else:
+                        print(f"{num} no es primo \n")
+            else:   
+                print("Por Favor, ingresar un valor valido")
         elif opcion == 12:
             #12. Sumar los digitos de un numero ingresado. Ejemplo: Si se ingresa 123, deberia devolver 6
-            num = int(input("Ingrese un numero "))
-            result=0
-            while(num!=0):
-                result+=num%10
-                num=trunc(num/10)
-            print(f"El resultado es: {result} \n")
+            num = input("Ingrese un numero ")
+            if(num.isdigit()):
+                num=int(num)
+                result=0 
+                while(num!=0):
+                    result+=num%10
+                    num=trunc(num/10)
+                print(f"El resultado es: {result} \n")
+            else:
+                print("Por Favor, ingresar un valor valido")
 
         elif opcion == 13:
             #13. Salir

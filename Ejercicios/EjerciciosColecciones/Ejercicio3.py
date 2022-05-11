e50e28230ef32a5039fe8eafe521f8d82b357fa7
vmachord <vane_divis@hotmail.es>
2022-05-11 11:26:28 +0200

    subir cambios

--- Ejercicios/EjerciciosColecciones/Ejercicio3.py
+++ Ejercicios/EjerciciosColecciones/Ejercicio3.py
@@ -4,28 +4,42 @@ lista_mujeres = []
 lista_hombres = []
 
 #llenar listas
-for i in range(1,101):
-    if(randint(0,1)==0):
-        lista_mujeres.append(randint(1,120))
-    else:
-        lista_hombres.append(randint(1,120))
+#for i in range(1,101):
+#    if(randint(0,1)==0):
+#        lista_mujeres.append(randint(1,120))
+#    else:
+#        lista_hombres.append(randint(1,120))
+
+#reemplazo lo anterior
+genero = [randint(0,1) for n in range(1,101)]
+lista_mujeres = [randint(1,120) for g in genero if g==1]
+lista_hombres = [randint(1,120) for g in genero if g==0]
+
+lista_mujeres.sort()
+lista_hombres.sort()
 
 print(lista_mujeres)
 print(lista_hombres)
 
-promedio_hombres=0
-promedio_mujeres=0
-mujeres_mayor_edad=0
+mujeres_mayor_edad = len([i for i in lista_mujeres if i>=18])
+hombres_menor_edad = len([i for i in lista_hombres if i<18])
 
-for i in lista_mujeres:
-    promedio_mujeres+=i
-    if(i>=18):
-        mujeres_mayor_edad+=1
+#for i in lista_mujeres:
+#    promedio_mujeres+=i
 
-for i in lista_hombres:
-    promedio_hombres+=i
+promedio_mujeres=sum(lista_mujeres)/len(lista_mujeres)
+
+#for i in lista_hombres:
+#    promedio_hombres+=i
+
+promedio_hombres=sum(lista_hombres)/len(lista_hombres)
 
 print(f"Existe una cantidad de {mujeres_mayor_edad} mujeres mayores de edad")
-print(f"El promedio de edad de mujeres es: {promedio_mujeres//len(lista_mujeres)}")
-print(f"El promedio de edad de hombres es: {promedio_hombres//len(lista_hombres)}")
+print(f"Existe una cantidad de {hombres_menor_edad} hombres menores de edad")
+print(f"Mujer con menor edad:{lista_mujeres[0]}")
+print(f"Mujer con mayor edad:{lista_mujeres[len(lista_mujeres)-1]}")
+print(f"Hombre con menor edad:{lista_hombres[0]}")
+print(f"Hombre con mayor edad:{lista_hombres[len(lista_hombres)-1]}")
+print(f"El promedio de edad de mujeres es: {promedio_mujeres}")
+print(f"El promedio de edad de hombres es: {promedio_hombres}")
 
